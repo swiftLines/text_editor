bd76240df67be043c4be8f7f94a8f8ece61060d2
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-08 20:00:21 -0500

    create toolbar and save, open buttons

--- text_editor.py
+++ text_editor.py
@@ -9,6 +9,14 @@ def main():
   text_edit = tk.Text(window, font='Helvetica 18')
   text_edit.grid(row=0, column=1)
 
+  frame = tk.Frame(window, relief=tk.Raised, bd=2)
+  save_button = tk.Button(frame, text="Save")
+  open_button = tk.Button(frame, text="Open")
+
+  save_button.grid(row=0, column=0)
+  open_button.grid(row=1, column=0)
+  frame.grid(row=0, column=0, sticky="ns")
+
   window.mainloop()
 
 main()
