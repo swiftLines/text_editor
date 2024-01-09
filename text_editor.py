939ae924888328e78cd2abc0e0e5743dd0bca959
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-08 19:51:24 -0500

    set column and row size

--- text_editor.py
+++ text_editor.py
@@ -3,6 +3,8 @@ from tkinter import *
 def main():
   window = tk.Tk()
   window.title('Text Editor')
+  window.rowconfigure(0, minsize=400)
+  window.columnconfigure(1, minsize=500)
 
   text_edit = tk.Text(window, font='Helvetica 18')
   text_edit.grid(row=0, column=1)
