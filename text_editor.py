508266bfde952bdc6573fb63e9f8e4d75e50c12e
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-08 19:47:50 -0500

    set text

--- text_editor.py
+++ text_editor.py
@@ -1,7 +1,12 @@
-import tkinter as tk
+from tkinter import *
 
 def main():
   window = tk.Tk()
   window.title('Text Editor')
 
+  text_edit = tk.Text(window, font='Helvetica 18')
+  text_edit.grid(row=0, column=1)
+
   window.mainloop()
+
+main()
